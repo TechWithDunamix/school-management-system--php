f63865c9d71fb91528d097caf00145998d0b8880
ryan-zayne <phoenixastro97@gmail.com>
2024-10-06 20:05:48 +0100

    fix: update database credentials and remove school name from item titles

--- js/custom/lib.js
+++ js/custom/lib.js
@@ -11,14 +11,16 @@ export const select = (selector, context = document) => context.querySelector(se
  */
 export const selectAll = (selector, context = document) => context.querySelectorAll(selector);
 
-import { createFetchClient } from "https://esm.run/@zayne-labs/callapi@0.7.9";
+import { createFetchClient } from "https://esm.run/@zayne-labs/callapi@rc";
 
 export const callApi = createFetchClient({
-	onResponseError: ({ response }) => {
-		if (response.status === 401) {
-			window.location.href = "login.html";
-		}
-	},
+	onResponseError: [
+		({ response }) => {
+			if (response.status === 401) {
+				window.location.href = "login.html";
+			}
+		},
+	],
 });
 
 import "https://cdn.twind.style";
